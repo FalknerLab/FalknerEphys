b775a45117c16e4529bbb570cb642d660ce667a2
DaveAndHisMice <da9769@princeton.edu>
2024-04-04 10:57:28 -0400

    made setup and toml file for package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    reqs = f.readlines()
+
+setup(
+    name="falknerephys",
+    version="0.0.1",
+    description="Analyses code for Falkner Lab silicon probe recordings",
+    url="https://github.com/FalknerLab/FalknerEphys.git",
+    author="David Allen, Nancy Mack, Bartul Mimica",
+    author_email="falknermice@gmail.com",
+    packages=['falknerephys'],
+    python_requires=">=3.8",
+    install_requires=reqs,
+    license_files=("LICENCE",),
+    license="BSD-3 Licence")
