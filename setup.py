c3189379f86200c81bfb4ba01c6c39085a09f8e4
DaveAndHisMice <da9769@princeton.edu>
2024-04-09 14:58:08 -0400

    added submodules to setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     url="https://github.com/FalknerLab/FalknerEphys.git",
     author="David Allen, Nancy Mack, Bartul Mimica",
     author_email="falknermice@gmail.com",
-    packages=['falknerephys'],
+    packages=['falknerephys', 'falknerephys.io', 'falknerephys.demos'],
     python_requires=">=3.8",
     install_requires=reqs,
     license_files=("LICENCE",),
