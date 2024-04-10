bb2e2b28834522df83ae33a57c42d8e1bbd6bedd
DaveAndHisMice <da9769@princeton.edu>
2024-04-09 23:09:21 -0400

    setup works with just MANIFEST

--- setup.py
+++ setup.py
@@ -3,27 +3,6 @@ from setuptools import find_namespace_packages, setup
 with open("requirements.txt") as f:
     reqs = f.readlines()
 
-def list_data():
-    data_files = ['falknerephys/demos/wm/phy/*.npy',
-                  'falknerephys/demos/wm/wm_demo_DAQ.h5',
-                  'falknerephys/demos/wm/wm_demo_SLEAP.h5']
-    return data_files
-
-
-# setup(
-#     name="falknerephys",
-#     version="0.0.1",
-#     description="Analyses code for Falkner Lab silicon probe recordings",
-#     url="https://github.com/FalknerLab/FalknerEphys.git",
-#     author="David Allen, Nancy Mack, Bartul Mimica",
-#     author_email="falknermice@gmail.com",
-#     packages=find_namespace_packages(),
-#     package_data={'falknerephys': list_data()},
-#     python_requires=">=3.8",
-#     install_requires=reqs,
-#     license_files=("LICENCE",),
-#     license="BSD-3 Licence")
-
 setup(
     name="falknerephys",
     version="0.0.1",
