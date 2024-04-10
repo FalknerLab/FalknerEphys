5c2bdb98a57519367938f315842c908b894a9920
DaveAndHisMice <da9769@princeton.edu>
2024-04-09 22:53:24 -0400

    paths to wm data in setup file refactored

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ from setuptools import find_namespace_packages, setup
 with open("requirements.txt") as f:
     reqs = f.readlines()
 
+def list_data():
+    data_files = ['falknerephys/demos/wm/phy/*.npy',
+                  'falknerephys/demos/wm/wm_demo_DAQ.h5',
+                  'falknerephys/demos/wm/wm_demo_SLEAP.h5']
+    return data_files
+
+
 setup(
     name="falknerephys",
     version="0.0.1",
@@ -11,8 +18,7 @@ setup(
     author="David Allen, Nancy Mack, Bartul Mimica",
     author_email="falknermice@gmail.com",
     packages=find_namespace_packages(),
-    package_dir={'wm': 'falknerephys/demos/wm'},
-    package_data={'wm': ['phy/*.npy', 'wm_demo_DAQ.h5', 'wm_demo_SLEAP.h5']},
+    package_data={'falknerephys': list_data()},
     python_requires=">=3.8",
     install_requires=reqs,
     license_files=("LICENCE",),
