976c4ceb06465a21a31e53657d14e4421636c8e5
DaveAndHisMice <da9769@princeton.edu>
2024-04-09 22:48:36 -0400

    paths to wm data in setup file

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ setup(
     author="David Allen, Nancy Mack, Bartul Mimica",
     author_email="falknermice@gmail.com",
     packages=find_namespace_packages(),
+    package_dir={'wm': 'falknerephys/demos/wm'},
+    package_data={'wm': ['phy/*.npy', 'wm_demo_DAQ.h5', 'wm_demo_SLEAP.h5']},
     python_requires=">=3.8",
     install_requires=reqs,
     license_files=("LICENCE",),
