b50378b14aa6ad28e7f5f007f9a62c3d1bf3d79d
DaveAndHisMice <da9769@princeton.edu>
2026-05-14 11:16:54 -0400

    multiple tiff generator functions for different scope imaging protocols. function to manually segment shanks for registering channels. commenting out Allen Atlas data importing because their SDK messes up our dependencies

--- falknerephys/io/register.py
+++ falknerephys/io/register.py
@@ -3,31 +3,57 @@ import os
 import subprocess
 import re
 
+import cv2
 import numpy as np
 import tifffile as tiff
-from dask.array.random import permutation
-from networkx.algorithms.approximation.traveling_salesman import traveling_salesman_problem
-from networkx.algorithms.cycles import find_cycle
-from networkx.algorithms.dag import dag_longest_path
-from networkx.algorithms.isomorphism.isomorphvf2 import DiGMState
-from networkx.algorithms.shortest_paths.generic import all_shortest_paths
-from networkx.algorithms.simple_paths import all_simple_paths
-from networkx.classes import DiGraph
 from scipy.ndimage import binary_dilation
 from scipy.spatial import distance_matrix
 from sklearn.cluster import AgglomerativeClustering
-from sklearn.neighbors import NearestNeighbors
-from scipy import stats
-from scipy.sparse.csgraph import shortest_path
 from brainrender import Scene
 from brainrender.actors import Line, Points, Volume, Point
 import matplotlib
 import matplotlib.pyplot as plt
-from allensdk.core.mouse_connectivity_cache import MouseConnectivityCache
+# from allensdk.core.mouse_connectivity_cache import MouseConnectivityCache
 import networkx as nx
+from networkx.algorithms.approximation.traveling_salesman import traveling_salesman_problem
+import requests
+import gzip
 
 
-def make_tiff_stack(tiff_prefix, out_fold, data_folder, chans=None):
+def make_tiff_stack(root_fold, out_fold=None, tiff_prefix='braintiff', chans=None, scope='lv'):
+    if out_fold is None:
+        out_fold = root_fold
+    match scope:
+        case 'lv':
+            make_tiff_lv(tiff_prefix, out_fold, root_fold, chans)
+        case 'ss':
+            make_tiff_ss(root_fold)
+
+
+def make_tiff_ss(tiff_prefix, root_fold, out_fold, scale_fac=0.5):
+    w, h = 0, 0
+    im_files = os.listdir(root_fold)
+    num_ims = len(im_files)
+    out_tiff = None
+    this_im = None
+    for i, f in enumerate(im_files):
+        print(i)
+        this_im = tiff.imread(os.path.join(root_fold, f), is_ome=False)
+        if i == 0:
+            w, h = this_im.shape
+            if scale_fac != 1:
+                w, h = int(w * scale_fac), int(h * scale_fac)
+            out_tiff = np.zeros((num_ims, 1, int(w * scale_fac), int(h * scale_fac)), dtype=this_im.dtype)
+        if scale_fac != 1:
+            rs_im = this_im.copy()
+            rs_im = cv2.resize(rs_im, (int(h * scale_fac), int(w * scale_fac)))
+            this_im = rs_im
+        out_tiff[i, :, :, :] = this_im[None, None, :, :]
+    out_name = os.path.join(out_fold, tiff_prefix + ".tiff")
+    tiff.imwrite(out_name, out_tiff, dtype=this_im.dtype, imagej=True)
+
+
+def make_tiff_lv(tiff_prefix, data_folder, out_fold, chans=None):
     if chans is None:
         chans = [0]
     all_ims = os.listdir(data_folder)
@@ -35,7 +61,7 @@ def make_tiff_stack(tiff_prefix, out_fold, data_folder, chans=None):
     for c in chans:
         chan_ims = []
         for im in all_ims:
-            chan_num = re.findall("Filter\d\d\d\d", im)
+            chan_num = re.findall(r"Filter\d\d\d\d", im)
             chan_name = re.findall("C00", im)
             if len(chan_num) == 1 and len(chan_name) == 1:
                 chan_num = chan_num[0]
@@ -54,22 +80,23 @@ def make_tiff_stack(tiff_prefix, out_fold, data_folder, chans=None):
         tiff.imwrite(out_name, out_tiff, dtype=im0.dtype, imagej=True)
 
 
-def register_brain(tiff_stack, out_dir, vox_dims=None, orientation='sal', atlas='auto'):
+def register_brain(tiff_stack, out_dir, vox_dims=None, orientation='sal', atlas='auto', bg_stack=None):
     if vox_dims is None:
         vox_dims = [10, 5.91, 5.91]
     if atlas == 'auto':
         atlas = 'allen_mouse_25um'
     reg_tiff = os.path.join(out_dir, 'downsampled_standard.tiff')
-    if not os.path.isfile(reg_tiff):
-        br_command = f'brainreg {tiff_stack} {out_dir} -v {vox_dims[0]} {vox_dims[1]} {vox_dims[2]} --orientation {orientation} --atlas {atlas}'
-        subprocess.run(br_command)
-    else:
+    br_command = f'brainreg {tiff_stack} {out_dir} -v {vox_dims[0]} {vox_dims[1]} {vox_dims[2]} --orientation {orientation} --atlas {atlas}'
+    if os.path.isfile(reg_tiff):
         print('Registered data found. Skipping Brainreg...')
+    else:
+        if bg_stack is not None:
+            br_command += f' --pre-processing skip --freeform-n-steps 2 --freeform-use-n-steps 1'
+        subprocess.run(br_command)
     return reg_tiff
 
 
-
-def segment_tracks(atlas_reg_tiff, samp_pt_scale=10, shanks_thresh=350, poly_deg=1, shank_ord='PostAnt', vox_sz=25,
+def auto_segment_tracks(atlas_reg_tiff, samp_pt_scale=10, shanks_thresh=350, poly_deg=1, shank_ord='PostAnt', vox_sz=25,
                     save_path=None, npx_chan_file=None, shank_box=25):
 
 
@@ -145,8 +172,55 @@ def segment_tracks(atlas_reg_tiff, samp_pt_scale=10, shanks_thresh=350, poly_deg
     return save_path
 
 
+def manual_segment_tracks(atlas_reg_tiff, num_shanks=4, shank_ord='PostAnt', save_path=None, npx_chan_file=None, shanks_thresh=350, vox_sz=25):
 
-def show_shank_tracks(shank_data_file, return_brain=False, brain=None, tiff_path=None, show_sig=True, show_lines=True,
+    tiff_vol = tiff.imread(atlas_reg_tiff) > shanks_thresh
+    topdown = np.sum(tiff_vol, axis=1)
+    sideview = np.sum(tiff_vol, axis=2)
+    plt.figure()
+    plt.imshow(topdown)
+    ap_ml = plt.ginput(2*num_shanks, timeout=0)
+    plt.figure()
+    plt.imshow(sideview)
+    tip_ap_dv = plt.ginput(2*num_shanks, timeout=0)
+
+    tops_ap = np.array(ap_ml)[:4, 1]
+    ml_top = np.array(ap_ml)[:4, 0]
+    ml_tip = np.array(ap_ml)[4:, 0]
+    tips_ap = np.array(tip_ap_dv)[4:, 1]
+    tips_dv = np.array(tip_ap_dv)[4:, 0]
+    tops_dv = np.array(tip_ap_dv)[:4, 0]
+
+    shank_tips = tips_dv
+    slope_ap = (tips_ap - tops_ap)/(tips_dv - tops_dv)
+    slope_ml = (ml_tip - ml_top)/(tips_dv - tops_dv)
+    y_ints_ap = tips_ap - tips_dv*slope_ap
+    y_ints_ml = ml_tip - tips_dv * slope_ml
+
+    chan_xyz = np.array([])
+    if npx_chan_file is not None:
+        chan_data = json.load(open(npx_chan_file))
+        chan_num = np.array(chan_data['chanMap'])
+        shank_ids = np.array(chan_data['kcoords']).astype(int)
+        depths = np.array(chan_data['yc'])
+        chan_xyz = []
+        for s in range(4):
+            chan_depths = vox_sz * shank_tips[s] - depths[shank_ids == s]
+            # chan_zs = np.repeat(vox_sz * ml[s], len(chan_depths))
+            chan_xs = vox_sz*(slope_ap[s]* chan_depths/vox_sz + y_ints_ap[s])
+            chan_zs = vox_sz * (slope_ml[s] * chan_depths / vox_sz + y_ints_ml[s])
+            chan_xyz.append(np.vstack((chan_num[shank_ids == s], chan_xs, chan_depths, chan_zs)).T)
+        chan_xyz = np.vstack(chan_xyz)
+
+    if save_path is not None:
+        save_path = os.path.join(save_path, 'shank_locations.npz')
+        np.savez(save_path, tip_dvs=shank_tips, shank_coefs=slope_ap, sig_thresh=shanks_thresh,
+                 tiff_path=atlas_reg_tiff, vox_size=vox_sz, chan_ccf=chan_xyz, method='manual')
+
+    return save_path
+
+
+def show_shank_tracks(shank_data_file, return_brain=False, brain=None, tiff_path=None, show_sig=True, show_lines=False,
                       show_label=True, chan_col='k', show_bounds=False):
     if brain is None:
         brain = Scene(atlas_name="allen_mouse_25um", title="Reconstructed Implant Locations")
@@ -203,7 +277,7 @@ def show_signal(registered_tiff, return_brain=False, brain=None, vox_sz=25, min_
         brain.render()
 
 
-def register_probes(tiff_path, probe_json=None, out_path=None, notrace=False, min_sig=350):
+def register_probes(tiff_path, probe_json=None, out_path=None, notrace=False, min_sig=350, auto=False):
     if out_path is None:
         out_path = os.path.join(os.path.split(tiff_path)[0], 'brainreg')
 
@@ -211,7 +285,10 @@ def register_probes(tiff_path, probe_json=None, out_path=None, notrace=False, mi
     if notrace:
         show_signal(registered_tiff, min_sig=min_sig)
     else:
-        shank_data_file = segment_tracks(registered_tiff, save_path=out_path, npx_chan_file=probe_json, shanks_thresh=min_sig)
+        if auto:
+            shank_data_file = auto_segment_tracks(registered_tiff, save_path=out_path, npx_chan_file=probe_json, shanks_thresh=min_sig)
+        else:
+            shank_data_file = manual_segment_tracks(registered_tiff, save_path=out_path, npx_chan_file=probe_json, shanks_thresh=min_sig)
         show_shank_tracks(shank_data_file)
 
 
@@ -236,21 +313,22 @@ def add_regions(region_list, brain=None, colors=None, alpha=0.25):
     return brain
 
 
-def add_allen_data(allen_exp_id, brain=None, vox_sz=25, min_density=0.25):
-    if brain is None:
-        brain = Scene()
+# def add_allen_data(allen_exp_id, brain=None, vox_sz=25, min_density=0.25):
+#     if brain is None:
+#         brain = Scene()
+#
+#     # tell the cache class what resolution (in microns) of data you want to download
+#     mcc = MouseConnectivityCache(resolution=vox_sz)
+#
+#     # download the projection density volume for one of the experiments
+#     pd = mcc.get_projection_density(allen_exp_id)
+#
+#     brain.add(Volume(pd[0], voxel_size=vox_sz, min_value=min_density, cmap='viridis'))
+#     return brain
 
-    # tell the cache class what resolution (in microns) of data you want to download
-    mcc = MouseConnectivityCache(resolution=vox_sz)
 
-    # download the projection density volume for one of the experiments
-    pd = mcc.get_projection_density(allen_exp_id)
-
-    brain.add(Volume(pd[0], voxel_size=vox_sz, min_value=min_density, cmap='viridis'))
-    return brain
-
-
-def project_svg(brain=None, use_silhouette=False, slice_pos=5000, slice_axis='sag', save_file=None, ax=None, show_brain=True):
+def project_svg(brain=None, use_silhouette=False, slice_pos=5000, slice_axis='sag', save_file=None, ax=None,
+                show_brain=True, alph=0.7):
     if brain is None:
         brain = Scene()
     if ax is None:
@@ -283,6 +361,10 @@ def project_svg(brain=None, use_silhouette=False, slice_pos=5000, slice_axis='sa
                     pts = splane.vertices[:, [pt_inds[ax_ind][0], pt_inds[ax_ind][1]]]
                     pt_cols = splane.pointcolors / 255
                     ax.scatter(pts[:, 0], pts[:, 1], c=pt_cols)
+                elif a.br_class == 'Point':
+                    pts = splane.coordinates[:, [pt_inds[ax_ind][0], pt_inds[ax_ind][1]]]
+                    clean_pts = make_shapes(pts)
+                    ax.fill(clean_pts[:, 0], clean_pts[:, 1], c=a.color(), alpha=alph, linewidth=0)
                 else:
                     splane = splane.boundaries()
                     pts = splane.coordinates[:, [pt_inds[ax_ind][0], pt_inds[ax_ind][1]]]
@@ -290,6 +372,7 @@ def project_svg(brain=None, use_silhouette=False, slice_pos=5000, slice_axis='sa
                     ax.plot(clean_pts[:, 0], clean_pts[:, 1], 'k')
     if save_file is not None:
         plt.savefig(save_file)
+    return ax
 
 
 def make_shapes(points, max_dist=250):
