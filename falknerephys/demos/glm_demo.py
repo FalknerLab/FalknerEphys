58280a5429e4125fcf43477dee3d928a56f60945
DaveAndHisMice <da9769@princeton.edu>
2026-07-02 12:50:22 -0400

    updated demos, function and import names needed refactoring

--- falknerephys/demos/glm_demo.py
+++ falknerephys/demos/glm_demo.py
@@ -1,16 +1,14 @@
 import matplotlib.pyplot as plt
-from falknerephys.preprocess import bin_fr, square_fr, gaus_fr, spikes_to_timeseries
+from falknerephys.spikesort import gaus_fr, spikes_to_timeseries
 from falknerephys.demos import wm_import_demo as wmd
-from falknerephys.modeling import fit_glm
-from sklearn.linear_model import TweedieRegressor
+from falknerephys.modeling import run_rc_glm
 
 
 def run_demo(ephys_data=None, behavior_data=None, b_hz=40):
     ts, fr_hm = spikes_to_timeseries(ephys_data, smooth_func=gaus_fr, time_win_ms=50, ts_len_s=int(len(behavior_data)/b_hz))
     for u in fr_hm.T:
         f, axs = plt.subplots(2, 1)
-        glm = TweedieRegressor()
-        y_test, pred, full_pred, r2, model = fit_glm(u, behavior_data, model=glm)
+        y_test, pred, full_pred, r2, model = run_rc_glm(u, behavior_data)
         axs[0].plot(ts, behavior_data)
         axs[0].plot(ts, full_pred)
         axs[1].plot(ts, u)
--- falknerephys/demos/wm_import_demo.py
+++ falknerephys/demos/wm_import_demo.py
@@ -1,10 +1,10 @@
+import h5py
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 import numpy as np
-from falknerephys.preprocess import get_time_win, bin_fr, square_fr, gaus_fr, spikes_to_timeseries
+from falknerephys.spikesort import get_time_win, bin_spikes, square_fr, gaus_fr, spikes_to_timeseries
 from falknerephys.io import whitematter as wm
 from falknerephys.plotting import fr_heatmap, set_labels, jitter_plot, fr_per_xy
-from falknerephys.behavior import get_sleap_data
 import os
 
 
@@ -47,7 +47,7 @@ def run_demo(path=None, f=None, example_id=41):
     jit_ax.set_title('Unit ' + ex_u + ' Spikes')
 
     # Preprocessing example plots
-    proc_funcs = [bin_fr, square_fr, gaus_fr]
+    proc_funcs = [bin_spikes, square_fr, gaus_fr]
     method = ['binned', 'moving average', 'gaussian']
     ylabs = ['# Spikes', 'Hz', 'Hz']
     for ind, p in enumerate(proc_funcs):
@@ -99,6 +99,26 @@ def get_demo_data():
     x = x[f60s]
     y = y[f60s]
     return daq_data, filt_dict, t, x, y, v
+
+
+def xy_to_cm(xy, center_pt, px_per_cm):
+    rel_xy = xy - center_pt
+    rel_xy[:, 1] = -rel_xy[:, 1]
+    cm_x = rel_xy[:, 0] / px_per_cm
+    cm_y = rel_xy[:, 1] / px_per_cm
+    return cm_x, cm_y
+
+
+def get_sleap_data(slp_h5, origin_px, px_per_cm, hz=40):
+    slp_data = h5py.File(slp_h5, 'r')
+    mouse_data = slp_data['tracks'][0]
+    mouse_centroid = np.nanmean(mouse_data, axis=1).T
+    cm_x, cm_y = xy_to_cm(mouse_centroid, origin_px, px_per_cm)
+    rel_xy = np.vstack((cm_x, cm_y)).T
+    dist_vec = np.linalg.norm(rel_xy[1:, :] - rel_xy[:-1, :], axis=1) / px_per_cm
+    vel_vec = np.hstack(([0], dist_vec))*hz
+    time_vec = np.arange(len(cm_x)) / hz
+    return time_vec, cm_x, cm_y, vel_vec
 
 
 if __name__ == '__main__':
